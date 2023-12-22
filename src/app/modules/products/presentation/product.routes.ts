068fe395f48a7fa08ccbd5f09ce31931a5d998c0
Alexander Leiva <aleiva.gogostudios@gmail.com>
2023-12-22 10:56:48 -0600

    feat: product layout finished

---
+++ src/app/modules/products/presentation/product.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { HomeProductComponent } from './pages/home-product/home-product.component';
+
+export const productRoutes: Routes = [
+  {
+    path: '',
+    component: HomeProductComponent,
+  },
+];
